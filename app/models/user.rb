be3ff605b201f8f2cb2a4ecaa677806808e7dfe9
Miyayan-k <kentaro.miyayan2077hornet@gmail.com>
2021-02-11 22:48:01 +0900

    ひらがなと漢字の正規表現設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,10 @@ class User < ApplicationRecord
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
-
   KANA_REGEX = /\A[ァ-ヴ]+\z/.freeze
   validates_format_of [:first_kana, :last_kana], with: KANA_REGEX, message: 'は全角カタカナのみ使用できます'
+  NAME_REGEX = /\A[ぁ-ん一-龥々]+\z/.freeze
+  validates_format_of [:first_name, :last_name], with: NAME_REGEX, message: 'は全角漢字あるいは全角ひらがなのみ使用できます'
 
   validates :nickname,   presence: true
   validates :first_name, presence: true
