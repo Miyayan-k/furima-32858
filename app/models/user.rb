126bfeec20726994443454fa146b7d35ab5bf522
Miyayan-k <kentaro.miyayan2077hornet@gmail.com>
2021-02-11 22:28:22 +0900

    カタカナ正規表現設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
+  KANA_REGEX = /\A[ァ-ヴ]+\z/.freeze
+  validates_format_of [:first_kana, :last_kana], with: KANA_REGEX, message: 'は全角カタカナのみ使用できます'
+
   validates :nickname,   presence: true
   validates :first_name, presence: true
   validates :last_name,  presence: true
