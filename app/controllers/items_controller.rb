7afd12310cd6c58eaef5134f978d319e33153a19
Miyayan-k <kentaro.miyayan2077hornet@gmail.com>
2021-02-14 21:09:02 +0900

    createアクション定義エラーメッセージの日本語化

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,26 @@
 class ItemsController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
 
   def index
   end
 
   def new
-    
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+    end
   end
 
   private
-  def item_parameter
+  def item_params
     params.require(:item).permit(
-      :title, :description, :category_id, :state_id, :shipping_id, 
-      :consignor_area_id, :days_id, :price, :image
+      :title, :description, :category_id, :state_id, 
+      :shipping_id, :consignor_area_id, :days_id, :price, :image
     )
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,6 @@ class Item < ApplicationRecord
   PRICE_REGEX = /\A[0-9]+\z/.freeze
   validates_format_of [:price], with: PRICE_REGEX, message: 'は半角数字のみ使用できます'
 
-
   with_options presence: true do
     validates :title
     validates :description
@@ -12,8 +11,7 @@ class Item < ApplicationRecord
     validates :shipping_id
     validates :consignor_area_id
     validates :days_id
-    validates :price, numericality: { 
-      only_integer: true, greater_than: 300, less_than: 9999999}
+    validates :price, numericality: { only_integer: true, greater_than: 300, less_than: 9999999}
     validates :image
   end
 
