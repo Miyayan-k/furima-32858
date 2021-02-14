c56ff9eb09c4dae5d48e90687bb55c0c174e80c8
Miyayan-k <kentaro.miyayan2077hornet@gmail.com>
2021-02-14 18:40:15 +0900

    ActiveStorage導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,12 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+  def item_parameter
+    params.require(:item).permit(
+      :title, :description, :category_id, :state_id, :shipping_id, 
+      :consignor_area_id, :days_id, :price, :image
+    )
+  end
 end
