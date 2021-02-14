f836df291d5a84cce796debf395554f2d13baac3
Miyayan-k <kentaro.miyayan2077hornet@gmail.com>
2021-02-14 19:22:09 +0900

    ログインしたユーザーにのみ出品ボタン表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
+  resources :items
 
   # ログアウト実行時に「No route matches [GET] "/users/sign_out"」エラーが発生するため
   devise_scope :user do
