b189f02436d11fcbef7d9430c5cda5572a0b9b87
Miyayan-k <kentaro.miyayan2077hornet@gmail.com>
2021-02-20 17:25:33 +0900

    addressモデルのバリデーション設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,12 +1,3 @@
 class Address < ApplicationRecord
-  with_options presence: true do
-    validates :post_number
-    validates :city
-    validates :street_number
-    validates :tel
-  end
-
-  validates :prefecture_id, numericality: { other_than: 0 }
-
   belongs_to :purchase
 end
