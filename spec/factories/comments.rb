a45d5ad5211add9544c65fa1c4df5f6d7471c343
Miyayan-k <kentaro.miyayan2077hornet@gmail.com>
2021-03-01 22:36:39 +0900

    コメントの単体テストコード実装

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :comment do
-    
+    text { Faker::Lorem.sentence }
+    association :user, :item
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+    @comment = FactoryBot.build(:comment, user_id: user.id, item_id: item.id)
+  end
+
+  it 'textがないとコメントは保存できない' do
+    @comment.text=''
+    @comment.valid?
+    expect(@comment.errors.full_messages).to include('コメントが入力されていません。')
+  end
+
+  it 'ユーザーが紐づいていないとコメントは保存できない' do
+    @comment.user = nil
+    @comment.valid?
+    expect(@comment.errors.full_messages).to include('ユーザーを入力してください')
+  end
+  it '商品が紐づいていないとコメントは保存できない' do
+    @comment.item = nil
+    @comment.valid?
+    expect(@comment.errors.full_messages).to include('商品を入力してください')
+  end
 end
